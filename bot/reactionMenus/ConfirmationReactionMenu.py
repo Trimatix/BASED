d40f9424858c6642282a92aa738ea1ac7b03f9dd
Trimatix <1jasperlaw@gmail.com>
2021-01-04 13:28:12 +0000

    add InlineConfirmationMenu

---
+++ bot/reactionMenus/ConfirmationReactionMenu.py
@@ -0,0 +1,16 @@
+from . import ReactionMenu
+from discord import Message, Member, User, Colour
+from typing import Dict, Union, List
+from .. import lib
+from ..cfg import cfg
+
+
+class InlineConfirmationMenu(ReactionMenu.InlineReactionMenu):
+    def __init__(self, msg : Message, targetMember : Union[Member, User], timeoutSeconds : int,
+                    titleTxt : str = "", desc : str = "", col : Colour = Colour.blue(), footerTxt : str = "", img : str = "",
+                    thumb : str = "", icon : str = "", authorName : str = ""):
+        
+        super().__init__(msg, targetMember, timeoutSeconds, options={cfg.defaultAcceptEmoji: ReactionMenu.DummyReactionMenuOption("Yes", cfg.defaultAcceptEmoji),
+                                                                        cfg.defaultRejectEmoji: ReactionMenu.DummyReactionMenuOption("No", cfg.defaultRejectEmoji)},
+                            returnTriggers=[cfg.defaultAcceptEmoji, cfg.defaultRejectEmoji], titleTxt=titleTxt, desc=desc, col=col, footerTxt=footerTxt,
+                            img=img, thumb=thumb, icon=icon, authorName=authorName)
