58aa9fca44722b144a93305476addb630cc94e7b
Trimatix <1jasperlaw@gmail.com>
2021-01-10 22:24:54 +0000

    ensure bot config is initialized before running

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
-from bot.cfg.configurator import loadCfg
+from bot.cfg import configurator
 
+# Load config if one is given
 if len(sys.argv) > 1:
-    loadCfg(sys.argv[1])
+    configurator.loadCfg(sys.argv[1])
 
+# initialize bot config
+configurator.init()
+
+# load and run bot
 from bot import bot
-
 status = bot.run()
 
+# return exit status code for bot restarting
 sys.exit(status)
