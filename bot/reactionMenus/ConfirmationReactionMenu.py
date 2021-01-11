d82545eafdd285f364ad73ba2a17a9cf58ed4319
Trimatix <1jasperlaw@gmail.com>
2021-01-11 23:38:24 +0000

    refactor ConfirmationReactionMenu

--- bot/reactionMenus/ConfirmationReactionMenu.py
+++ bot/reactionMenus/ConfirmationReactionMenu.py
@@ -5,11 +5,13 @@ from ..cfg import cfg
 
 
 class InlineConfirmationMenu(ReactionMenu.InlineReactionMenu):
-    def __init__(self, msg : Message, targetMember : Union[Member, User], timeoutSeconds : int,
-                    titleTxt : str = "", desc : str = "", col : Colour = Colour.blue(), footerTxt : str = "", img : str = "",
-                    thumb : str = "", icon : str = "", authorName : str = ""):
-        
-        super().__init__(msg, targetMember, timeoutSeconds, options={cfg.defaultEmojis.accept: ReactionMenu.DummyReactionMenuOption("Yes", cfg.defaultEmojis.accept),
-                                                                        cfg.defaultEmojis.reject: ReactionMenu.DummyReactionMenuOption("No", cfg.defaultEmojis.reject)},
-                            returnTriggers=[cfg.defaultEmojis.accept, cfg.defaultEmojis.reject], titleTxt=titleTxt, desc=desc, col=col, footerTxt=footerTxt,
-                            img=img, thumb=thumb, icon=icon, authorName=authorName)
+    def __init__(self, msg: Message, targetMember: Union[Member, User], timeoutSeconds: int,
+                 titleTxt: str = "", desc: str = "", col: Colour = Colour.blue(), footerTxt: str = "", img: str = "",
+                 thumb: str = "", icon: str = "", authorName: str = ""):
+
+        options = {cfg.defaultEmojis.accept: ReactionMenu.DummyReactionMenuOption("Yes", cfg.defaultEmojis.accept),
+                    cfg.defaultEmojis.reject: ReactionMenu.DummyReactionMenuOption("No", cfg.defaultEmojis.reject)}
+
+        super().__init__(msg, targetMember, timeoutSeconds, options=options, img=img, thumb=thumb, icon=icon,
+                            authorName=authorName, returnTriggers=[cfg.defaultEmojis.accept, cfg.defaultEmojis.reject],
+                            titleTxt=titleTxt, desc=desc, col=col, footerTxt=footerTxt)
