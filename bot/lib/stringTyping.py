94ba6ad69b9e68740607e202d2153ea8aedea205
Trimatix <1jasperlaw@gmail.com>
2021-01-11 22:41:58 +0000

    refactor lib.stringTyping

--- bot/lib/stringTyping.py
+++ bot/lib/stringTyping.py
@@ -17,17 +17,19 @@ def isInt(x) -> bool:
     return True
 
 
-def isMention(mention : str) -> bool:
-    """Decide whether the given string is a discord user mention, being either <@USERID> or <@!USERID> where USERID is an integer discord user id.
+def isMention(mention: str) -> bool:
+    """Decide whether the given string is a discord user mention,
+    being either <@USERID> or <@!USERID> where USERID is an integer discord user id.
 
     :param str mention: The string to check
     :return: True if mention matches the formatting of a discord user mention, False otherwise
     :rtype: bool
     """
-    return mention.endswith(">") and ((mention.startswith("<@") and isInt(mention[2:-1])) or (mention.startswith("<@!") and isInt(mention[3:-1])))
+    return mention.endswith(">") and ((mention.startswith("<@") and isInt(mention[2:-1])) or \
+                                        (mention.startswith("<@!") and isInt(mention[3:-1])))
 
 
-def isRoleMention(mention : str) -> bool:
+def isRoleMention(mention: str) -> bool:
     """Decide whether the given string is a discord role mention, being <@&ROLEID> where ROLEID is an integer discord role id.
 
     :param str mention: The string to check
@@ -37,7 +39,7 @@ def isRoleMention(mention : str) -> bool:
     return mention.endswith(">") and mention.startswith("<@&") and isInt(mention[3:-1])
 
 
-def commaSplitNum(num : str) -> str:
+def commaSplitNum(num: str) -> str:
     """Insert commas into every third position in a string.
     For example: "3" -> "3", "30000" -> "30,000", and "561928301" -> "561,928,301"
 
@@ -52,10 +54,10 @@ def commaSplitNum(num : str) -> str:
 
 
 # string extensions for numbers, e.g 11th, 1st, 23rd...
-numExtensions = ["th","st","nd","rd","th","th","th","th","th","th"]
+numExtensions = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"]
 
 
-def getNumExtension(num : int) -> str:
+def getNumExtension(num: int) -> str:
     """Return the string extension for an integer, e.g 'th' or 'rd'.
 
     :param int num: The integer to find the extension for
