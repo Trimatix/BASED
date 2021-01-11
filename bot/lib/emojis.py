e31e686c5e79d2536dcf3a126208559cefba6e6b
Trimatix <1jasperlaw@gmail.com>
2021-01-11 22:38:14 +0000

    refactor lib.emojis

--- bot/lib/emojis.py
+++ bot/lib/emojis.py
@@ -16,7 +16,7 @@ raiseUnkownEmojis = True
 logUnknownEmojis = True
 
 
-def strIsUnicodeEmoji(c : str) -> bool:
+def strIsUnicodeEmoji(c: str) -> bool:
     """Decide whether a given string contrains a single unicode emoji.
 
     :param str c: The string to test
@@ -26,7 +26,7 @@ def strIsUnicodeEmoji(c : str) -> bool:
     return c in UNICODE_EMOJI
 
 
-def strIsCustomEmoji(s : str) -> bool:
+def strIsCustomEmoji(s: str) -> bool:
     """Decide whether the given string matches the formatting of a discord custom emoji,
     being <:NAME:ID> where NAME is the name of the emoji, and ID is the integer ID.
 
@@ -37,16 +37,16 @@ def strIsCustomEmoji(s : str) -> bool:
     if s.startswith("<") and s.endswith(">"):
         try:
             first = s.index(":")
-            second = first + s[first+1:].index(":") + 1
+            second = first + s[first + 1:].index(":") + 1
         except ValueError:
             return False
-        return stringTyping.isInt(s[second+1:-1])
+        return stringTyping.isInt(s[second + 1:-1])
     return False
 
 
 class BasedEmoji(serializable.Serializable):
-    """A class that really shouldnt be necessary, acting as a union over the str (unicode) and Emoji type emojis used and returned by discord.
-    To instance this class, provide exactly one of the constructor's keyword arguments.
+    """A class that really shouldnt be necessary, acting as a union over the str (unicode) and Emoji type emojis used
+    and returned by discord. To instance this class, provide exactly one of the constructor's keyword arguments.
 
     :var id: The ID of the Emoji that this object represents, if isID
     :vartype id: int
@@ -63,12 +63,12 @@ class BasedEmoji(serializable.Serializable):
     """
     EMPTY = None
 
-
-    def __init__(self, id : int = -1, unicode : str = "", rejectInvalid : bool = False):
+    def __init__(self, id: int = -1, unicode: str = "", rejectInvalid: bool = False):
         """
         :param int id: The ID of the custom emoji that this object should represent.
         :param str unicode: The unicode emoji that this object should represent.
-        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested, regardless of raiseUnknownEmojis (Default False)
+        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested,
+                                    regardless of raiseUnknownEmojis (Default False)
         """
 
         if id == -1 and unicode == "":
@@ -87,12 +87,14 @@ class BasedEmoji(serializable.Serializable):
         self.sendable = self.unicode if self.isUnicode else str(botState.client.get_emoji(self.id))
         if self.sendable == "None":
             if logUnknownEmojis:
-                botState.logger.log("BasedEmoji", "init", "Unrecognised custom emoji ID in BasedEmoji constructor: " + str(self.id), trace=traceback.format_exc())
+                botState.logger.log("BasedEmoji", "init", "Unrecognised custom emoji ID in BasedEmoji constructor: " +
+                                    str(self.id), trace=traceback.format_exc())
             if raiseUnkownEmojis or rejectInvalid:
-                raise exceptions.UnrecognisedCustomEmoji("Unrecognised custom emoji ID in BasedEmoji constructor: " + str(self.id),self.id)
+                raise exceptions.UnrecognisedCustomEmoji(
+                    "Unrecognised custom emoji ID in BasedEmoji constructor: " + str(self.id), self.id)
             self.sendable = err_UnknownEmoji
 
-    
+
     def toDict(self, **kwargs) -> dict:
         """Serialize this emoji to dictionary format for saving to file.
 
@@ -100,8 +102,8 @@ class BasedEmoji(serializable.Serializable):
         :rtype: dict
         """
         if self.isUnicode:
-            return {"unicode":self.unicode}
-        return {"id":self.id}
+            return {"unicode": self.unicode}
+        return {"id": self.id}
 
 
     def __repr__(self) -> str:
@@ -112,30 +114,31 @@ class BasedEmoji(serializable.Serializable):
         """
         return "<BasedEmoji-" + ("id" if self.isID else "unicode") + ":" + (str(self.id) if self.isID else self.unicode) + ">"
 
-    
+
     def __hash__(self) -> int:
-        """Calculate a hash of this emoji, based on its repr string. Two BasedEmoji objects representing the same emoji will have the same repr and hash.
+        """Calculate a hash of this emoji, based on its repr string.
+        Two BasedEmoji objects representing the same emoji will have the same repr and hash.
 
         :return: A hash of this emoji
         :rtype: int
         """
         return hash(repr(self))
 
-    
-    def __eq__(self, other : BasedEmoji) -> bool:
+
+    def __eq__(self, other: BasedEmoji) -> bool:
         """Decide if this BasedEmoji is equal to another.
         Two BasedEmojis are equal if they represent the same emoji (i.e ID/unicode) of the same type (custom/unicode)
-        
+
         :param BasedEmoji other: the emoji to compare this one to
         :return: True of this emoji is semantically equal to the given emoji, False otherwise
         :rtype: bool
         """
         return type(other) == BasedEmoji and self.sendable == other.sendable
 
-    
+
     def __str__(self) -> str:
         """Get the object's 'sendable' string.
-        
+
         :return: A string sendable to discord that will be translated into an emoji by the discord client.
         :rtype: str
         """
@@ -143,19 +146,21 @@ class BasedEmoji(serializable.Serializable):
 
 
     @classmethod
-    def fromDict(cls, emojiDict : dict, **kwargs) -> BasedEmoji:
+    def fromDict(cls, emojiDict: dict, **kwargs) -> BasedEmoji:
         """Construct a BasedEmoji object from its dictionary representation.
         If both an ID and a unicode representation are provided, the emoji ID will be used.
 
         TODO: If ID is -1, use unicode. If unicode is "", use ID.
 
-        :param dict emojiDict: A dictionary containing either an ID (for custom emojis) or a unicode emoji string (for unicode emojis)
-        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested, regardless of raiseUnknownEmojis (Default False)
+        :param dict emojiDict: A dictionary containing either an ID (for custom emojis) or
+                                a unicode emoji string (for unicode emojis)
+        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested,
+                                    regardless of raiseUnknownEmojis (Default False)
         :return: A new BasedEmoji object as described in emojiDict
         :rtype: BasedEmoji
         """
         rejectInvalid = kwargs["rejectInvalid"] if "rejectInvalid" in kwargs else False
-            
+
         if type(emojiDict) == BasedEmoji:
             return emojiDict
         if "id" in emojiDict:
@@ -165,10 +170,11 @@ class BasedEmoji(serializable.Serializable):
 
 
     @classmethod
-    def fromPartial(cls, e : PartialEmoji, rejectInvalid : bool = False) -> BasedEmoji:
+    def fromPartial(cls, e: PartialEmoji, rejectInvalid: bool = False) -> BasedEmoji:
         """Construct a new BasedEmoji object from a given discord.PartialEmoji.
 
-        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested, regardless of raiseUnknownEmojis (Default False)
+        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested,
+                                    regardless of raiseUnknownEmojis (Default False)
         :return: A BasedEmoji representing e
         :rtype: BasedEmoji
         """
@@ -181,12 +187,13 @@ class BasedEmoji(serializable.Serializable):
 
 
     @classmethod
-    def fromReaction(cls, e : Union[Emoji, PartialEmoji, str], rejectInvalid : bool = False) -> BasedEmoji:
+    def fromReaction(cls, e: Union[Emoji, PartialEmoji, str], rejectInvalid: bool = False) -> BasedEmoji:
         """Construct a new BasedEmoji object from a given discord.PartialEmoji, discord.Emoji, or string.
 
         :param e: The reaction emoji to convert to BasedEmoji
         :type e: Union[Emoji, PartialEmoji, str]
-        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested, regardless of raiseUnknownEmojis (Default False)
+        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested,
+                                    regardless of raiseUnknownEmojis (Default False)
         :return: A BasedEmoji representing e
         :rtype: BasedEmoji
         """
@@ -206,13 +213,15 @@ class BasedEmoji(serializable.Serializable):
 
 
     @classmethod
-    def fromStr(cls, s : str, rejectInvalid : bool = False) -> BasedEmoji:
+    def fromStr(cls, s: str, rejectInvalid: bool = False) -> BasedEmoji:
         """Construct a BasedEmoji object from a string containing either a unicode emoji or a discord custom emoji.
         s may also be a BasedEmoji (returns s), a dictionary-serialized BasedEmoji (returns BasedEmoji.fromDict(s)), or
         only an ID of a discord custom emoji (may be either str or int)
 
-        :param str s: A string containing only one of: A unicode emoji, a discord custom emoji, or the ID of a discord custom emoji.
-        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested, regardless of raiseUnknownEmojis (Default False)
+        :param str s: A string containing only one of: A unicode emoji, a discord custom emoji, or
+                        the ID of a discord custom emoji.
+        :param bool rejectInvalid: When true, an exception is guaranteed to raise if an invalid emoji is requested,
+                                    regardless of raiseUnknownEmojis (Default False)
         :return: A BasedEmoji representing the given string emoji
         :rtype: BasedEmoji
         """
@@ -223,7 +232,7 @@ class BasedEmoji(serializable.Serializable):
         if strIsUnicodeEmoji(s):
             return BasedEmoji(unicode=s, rejectInvalid=rejectInvalid)
         elif strIsCustomEmoji(s):
-            return BasedEmoji(id=int(s[s[s.index(":")+1:].index(":")+3:-1]), rejectInvalid=rejectInvalid)
+            return BasedEmoji(id=int(s[s[s.index(":") + 1:].index(":") + 3:-1]), rejectInvalid=rejectInvalid)
         elif stringTyping.isInt(s):
             return BasedEmoji(id=int(s), rejectInvalid=rejectInvalid)
         else:
@@ -231,7 +240,7 @@ class BasedEmoji(serializable.Serializable):
 
 
     @classmethod
-    def fromUninitialized(cls, e : UninitializedBasedEmoji) -> BasedEmoji:
+    def fromUninitialized(cls, e: UninitializedBasedEmoji) -> BasedEmoji:
         """Construct a BasedEmoji object from an UninitializedBasedEmoji object.
         Always throws an exception if the emoji is unrecognised.
 
@@ -248,7 +257,8 @@ class BasedEmoji(serializable.Serializable):
             return BasedEmoji.fromDict(e.value, rejectInvalid=True)
         # Unrecognised uninitialized value
         else:
-            raise ValueError("Unrecognised UninitializedBasedEmoji value type. Expecting int, str or dict, given '" + type(e.value).__name__ + "'")
+            raise ValueError("Unrecognised UninitializedBasedEmoji value type. Expecting int, str or dict, given '" +
+                                type(e.value).__name__ + "'")
 
 
 # 'static' object representing an empty/lack of emoji
@@ -263,8 +273,10 @@ class UninitializedBasedEmoji:
     No instances of this class should be present after bot client's on_ready event
     has finished executing.
     """
+
     def __init__(self, value):
         """
-        :param value: The data to attempt to initialize an emoji with. For example, an integer ID, or a string unicode character.
+        :param value: The data to attempt to initialize an emoji with. For example, an integer ID, or
+                        a string unicode character.
         """
         self.value = value
