e4b013172f521b1d31b0806144da86a1047c95a2
Trimatix <1jasperlaw@gmail.com>
2021-01-18 10:45:42 +0000

    when forceExpire-ing a task, mark it as expired before calling the expiry function rather than after.

--- bot/scheduling/TimedTask.py
+++ bot/scheduling/TimedTask.py
@@ -197,17 +197,19 @@ class TimedTask:
         """
         # Update expiryTime
         self.expiryTime = datetime.utcnow()
-        # Call expiryFunction and reschedule if specified
-        if callExpiryFunc and self.hasExpiryFunction:
-            expiryFuncResults = await self.callExpiryFunction()
-        else:
-            expiryFuncResults = None
 
         if self.autoReschedule:
             await self.reschedule()
         # Mark for removal if not rescheduled
         else:
             self.gravestone = True
+
+        # Call expiryFunction and reschedule if specified
+        if callExpiryFunc and self.hasExpiryFunction:
+            expiryFuncResults = await self.callExpiryFunction()
+        else:
+            expiryFuncResults = None
+
         # Return expiry function results
         if callExpiryFunc and self.hasExpiryFunction:
             return expiryFuncResults
