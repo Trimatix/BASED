5852f2faa2734621ca6ad83974069e4f34facdad
Trimatix <1jasperlaw@gmail.com>
2021-02-15 00:20:57 +0000

    remove botState type hints to avoid import loops

--- bot/botState.py
+++ bot/botState.py
@@ -1,27 +1,18 @@
-from .bot import BasedClient
-from .databases.userDB import UserDB
-from .databases.guildDB import GuildDB
-from .databases.reactionMenuDB import ReactionMenuDB
-from .logging import Logger
-from .scheduling.timedTask import TimedTask
-from .scheduling.timedTaskHeap import TimedTaskHeap
-
-
 class ShutDownState:
     restart = 0
     shutdown = 1
     update = 2
 
 
-client: "BasedClient" = None
-shutdown: ShutDownState = ShutDownState.restart
+client = None
+shutdown = ShutDownState.restart
 
-usersDB: "UserDB" = None
-guildsDB: "GuildDB" = None
-reactionMenusDB: "ReactionMenuDB" = None
+usersDB = None
+guildsDB = None
+reactionMenusDB = None
 
-dbSaveTT: "TimedTask" = None
-updatesCheckTT: "TimedTask" = None
+dbSaveTT = None
+updatesCheckTT = None
 
-taskScheduler: "TimedTaskHeap" = None
-logger: "Logger" = None
+taskScheduler = None
+logger = None
